89adbc062bb35596a3447e30fc53c44b0ceab1d1
FearlessAtom <atomgotaggressive@gmail.com>
2025-03-14 21:34:59 +0200

    adding Singleton

---
+++ Singleton/Authenticator.cs
@@ -0,0 +1,29 @@
+﻿namespace Singleton;
+
+public sealed class Authenticator
+{
+    static private Authenticator? Instance;
+    static private object LockObject = new Object();
+
+    private Authenticator()
+    {
+        Console.WriteLine("Creating an instance of a class...");
+        Thread.Sleep(1500);
+    }
+
+    static public Authenticator GetIstance()
+    {
+        if (Instance == null)
+        {
+            lock(LockObject)
+            {
+                if (Instance == null)
+                {
+                    Instance = new Authenticator();
+                }
+            }
+        }
+
+        return Instance;
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Singleton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            (new Thread(() =>
+            {
+                 Authenticator Authenticator = Authenticator.GetIstance();
+            })).Start();
+        }
+
+        Authenticator a = Authenticator.GetIstance();
+        Authenticator b = Authenticator.GetIstance();
+
+        Console.WriteLine();
+        
+        Console.WriteLine("Equality: " + (a == b ? "True" : "False"));
+    }
+}
